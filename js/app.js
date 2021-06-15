9c7d1a1b2695eb5c5484576ae09007f2d1b01427
unknown <azizleyla01gmail.com>
2021-06-15 11:32:47 +0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.navbar__toggle');
+const menu = document.querySelector('.navbar__menu');
+btn.addEventListener('click', function () {
+    menu.classList.toggle('show')
+})
+
+
+
+
